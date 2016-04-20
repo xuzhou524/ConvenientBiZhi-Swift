1792de65ff7ad17b2bd0aa2d0bc9bd16fb3b6914
xuzhou524 <xuuohz@163.com>
2016-04-20 13:18:04 +0800

    添加home

--- ConvenientBiZhi-Swift/AppDelegate.swift
+++ ConvenientBiZhi-Swift/AppDelegate.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow()
+        self.window?.frame = UIScreen.mainScreen().bounds
+        self.window?.backgroundColor = UIColor.whiteColor()
+        self.window?.makeKeyAndVisible()
+        self.window?.rootViewController = HomeViewController()
+
         return true
     }
 
